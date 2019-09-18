8b94a395890fdd8469920cfdde19fc826f61b6e4
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-18 20:50:45 +0530

    refactor: minor refactoring of button stories code
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- tests/storybook/button.stories.js
+++ tests/storybook/button.stories.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { useColorMode } from "theme-ui";
 import Button from '../../src/components/button';
 import { storiesOf } from "@storybook/react";
 
 export default { title: 'Button' };
 
-storiesOf("Button", module).add("PrimaryButton", () => {
-  return (
-    <Button type="primary">
-      Primary Button
-    </Button>
-  )
-});
-
-/* export const PrimaryButton = () => (
-  <Button type="primary">
-    Primary Button
-  </Button>
-);
-
-export const SecondaryButton = () => (
-  <Button type="secondary">
-    Secondary Button
-  </Button>
-);*/  
+storiesOf("Light Mode|Button", module)
+  .add("Primary", () => {
+    return (
+      <Button type="primary">
+        Primary Button
+      </Button>
+    )
+  })
+  .add("Secondary", () => {
+    return (
+      <Button type="secondary">
+        Secondary Button
+      </Button>
+    )
+  });
