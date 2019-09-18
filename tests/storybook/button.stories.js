10a75d348b69481772bc0b072c40e79fa764cf8b
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-18 23:15:38 +0530

    chore: trying to fix color mode hook problem
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- tests/storybook/button.stories.js
+++ tests/storybook/button.stories.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useColorMode, ThemeProvider } from 'theme-ui';
+import index from "../../src/gatsby-plugin-theme-ui/index.js";
+
 import Button from '../../src/components/button';
 
 export default { 
   title: 'Light theme | Button' ,
   component: Button,
+  decorators: [
+    story => (
+      <ThemeProvider theme={index}>
+        {story()}
+      </ThemeProvider>
+    ),
+  ],
 };
 
 export const Primary = () => {
+  const [colorMode, setColorMode] = useColorMode();
   return (
     <Button type="primary">
       Primary Button
