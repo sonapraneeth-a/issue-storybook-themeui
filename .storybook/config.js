c9238ce9e22a82d4181b5f6eba2378226b71e6d7
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-18 17:38:48 +0530

    refactor: moved storybook code to tests/storybook
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,6 @@
 import { configure } from '@storybook/react';
 
-configure(require.context('../src', true, /\.stories\.js$/), module);
+configure(
+  require.context('../tests/storybook', true, /\.stories\.js$/), 
+  module
+);
